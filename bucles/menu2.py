2b03baf6f38862859078080e1405f6346a42a301
Miguel Loaiza <miguel.mejial@upb.edu.co>
2025-08-28 17:25:08 -0500

    se realizo un menu

---
+++ bucles/menu2.py
@@ -0,0 +1,21 @@
+print("1. Entradas\n2. Platos fuertes\n3. Bebidas\n4. Postres\n5. salir")
+opcion = int(input("Elija una opción:"))
+
+# Utilizamos una variable tipo booleana -> una bandera
+control = true 
+
+while control == true:
+    match opcion:
+    case 1: 
+     print("1. Patacón con hogao")¨
+     print("2. Yuca con chicharrón")
+     print("3. Guineo con Suero")
+    case 2:
+     print("1. Solomito")
+     print("2. Hamburguesa")
+     print("3. Sushi")
+    case 3:
+     print("1. Limonada")
+     print("2. Jugos")
+     print("3. Sushi")
+
