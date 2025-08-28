ac032c1f4e446955a55f955e7025ef04e64dc3d9
Miguel Loaiza <miguel.mejial@upb.edu.co>
2025-08-28 16:24:00 -0500

    se hizo primer ejercicio bucles

---
+++ bucles/ejerciciobucle.py
@@ -0,0 +1,11 @@
+numero = 1
+while numero >= 0:
+    print(numero)
+    numero += 1            #numero = numero + 1
+
+#Imprimir los numeros pares entre el 20 y el 80
+numero = 20
+while numero <= 80:
+    if numero % 2 == 0:
+        print(numero)
+    numero += 1
