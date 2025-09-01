29300f1c83a72cca37e17c9af2630f632ed2ef66
Miguel Loaiza <miguel.mejial@upb.edu.co>
2025-09-01 16:23:50 -0500

    se realizo actividades pendientes

--- bucles/menu2.py
+++ bucles/menu2.py
@@ -1,21 +1,31 @@
-print("1. Entradas\n2. Platos fuertes\n3. Bebidas\n4. Postres\n5. salir")
-opcion = int(input("Elija una opción:"))
 
-# Utilizamos una variable tipo booleana -> una bandera
-control = true 
-
-while control == true:
+control = True
+while control == True:
+    print("1. Entradas\n2. Platos fuertes\n3. Bebidas\n4. Postres\n5. Salir")
+    opcion = int(input("Elija una opcion: "))
     match opcion:
-    case 1: 
-     print("1. Patacón con hogao")¨
-     print("2. Yuca con chicharrón")
-     print("3. Guineo con Suero")
-    case 2:
-     print("1. Solomito")
-     print("2. Hamburguesa")
-     print("3. Sushi")
-    case 3:
-     print("1. Limonada")
-     print("2. Jugos")
-     print("3. Sushi")
-
+        case 1:
+            print("1. Patacon con hogao")
+            print("2. Yuca con chicharron")
+            print("3. Guineo con suero")
+            print()
+        case 2:
+            print("1. Solomito")
+            print("2. Hamburguesa")
+            print("3. Sushi")
+            print()
+        case 3:
+            print("1. Limonada")
+            print("2. Jugos naturales")
+            print("3. Cerveza")
+            print()
+        case 4:
+            print("1. Tres leches")
+            print("2. Tiramusu")
+            print("3. Sushi")
+            print()
+        case 5:
+            control = False
+        case _:
+            print("Opcion no valida")
+            print()
